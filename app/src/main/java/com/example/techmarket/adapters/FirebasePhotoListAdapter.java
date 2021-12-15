ab29fc3e62be0bf56f7da618c0ec36a79d663734
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-12-15 06:13:16 +0300

    This commit extends the FirebaseRecyclerAdapter

--- app/src/main/java/com/example/techmarket/adapters/FirebasePhotoListAdapter.java
+++ app/src/main/java/com/example/techmarket/adapters/FirebasePhotoListAdapter.java
@@ -1,4 +1,26 @@
 package com.example.techmarket.adapters;
 
-public class FirebasePhotoListAdapter {
+import android.content.Context;
+
+import com.example.techmarket.models.InterestingPhoto;
+import com.example.techmarket.utils.ItemTouchHelperAdapter;
+import com.example.techmarket.utils.OnStartDragListener;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+
+public class FirebasePhotoListAdapter extends FirebaseRecyclerAdapter<InterestingPhoto, FirebasePhotoViewHolder> implements ItemTouchHelperAdapter {
+    private DatabaseReference mRef;
+    private OnStartDragListener mOnStartDragListener;
+    private Context mContext;
+
+    public FirebasePhotoListAdapter(FirebaseRecyclerOptions<InterestingPhoto> options,
+                                    DatabaseReference ref,
+                                    OnStartDragListener onStartDragListener,
+                                    Context context){
+        super(options);
+        mRef = ref.getRef();
+        mOnStartDragListener = onStartDragListener;
+        mContext = context;
+    }
 }

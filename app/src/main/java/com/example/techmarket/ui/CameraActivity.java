944089bec7738cd19d286e61ed5fc95581c449b0
Ckhajira <christopher.khajira@student.moringaschool.com>
2022-01-09 22:31:35 +0300

    This commit overrides the onActivityResult

--- app/src/main/java/com/example/techmarket/ui/CameraActivity.java
+++ app/src/main/java/com/example/techmarket/ui/CameraActivity.java
@@ -1,21 +1,29 @@
 package com.example.techmarket.ui;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.techmarket.R;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
     public static final int CAMERA_REQUEST_CODE = 102;
@@ -71,6 +79,38 @@ public class CameraActivity extends AppCompatActivity {
                 Toast.makeText(this, "Camera Permission is Required to use Camera.", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == CAMERA_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                File f =new File(currentPhotoPath);
+//                selectedImage.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "Absolute Url of image is" + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+
+                uploadImageToFirebase(f.getName(),contentUri);
+            }
+        }
+        if(requestCode == GALLERY_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" +timeStamp + "."+getFileExt(contentUri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  " + imageFileName);
+//                selectedImage.setImageURI(contentUri);
+
+                uploadImageToFirebase(imageFileName,contentUri);
+            }
+        }
+
+        super.onActivityResult(requestCode,resultCode,data);
     }
 
 

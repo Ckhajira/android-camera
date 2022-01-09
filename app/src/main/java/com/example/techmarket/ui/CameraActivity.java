2af853503313b9338662043e0e1fd960a3fa7ecc
Ckhajira <christopher.khajira@student.moringaschool.com>
2022-01-09 22:25:35 +0300

    This commit adds the fields needed

--- app/src/main/java/com/example/techmarket/ui/CameraActivity.java
+++ app/src/main/java/com/example/techmarket/ui/CameraActivity.java
@@ -3,10 +3,19 @@ package com.example.techmarket.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.techmarket.R;
 
 public class CameraActivity extends AppCompatActivity {
+    public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
+    ImageView selectedImage;
+    Button cameraBtn,galleryBtn;
+    public static final int CAMERA_PERM_CODE = 101;
+    String currentPhotoPath;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
